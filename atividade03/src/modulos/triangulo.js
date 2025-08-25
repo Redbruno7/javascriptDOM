3af37775de770ff0ada8a8a652e1c2956b173b42
Redbruno7 <brunorodgers7@gmail.com>
2025-08-25 14:01:30 -0300

    A03 - R01

--- atividade03/src/modulos/triangulo.js
+++ atividade03/src/modulos/triangulo.js
@@ -1,9 +1,9 @@
-export function verificarTriangulo (a, b, c, pTriangulo) {
+export function verificarTriangulo(a, b, c, pTriangulo) {
     if (isNaN(a) || isNaN(b) || isNaN(c)) {
-        pTriangulo.textContent = "Coloque valores válidos"
-    } else if (a + b > c || b + c > a || a + c > b) {
-        pTriangulo.textContent = "É possivel fazer um triangulo com estes valores"
+        pTriangulo.textContent = "Coloque valores válidos";
+    } else if (a + b > c && b + c > a && a + c > b) {
+        pTriangulo.textContent = "É possível fazer um triângulo com estes valores";
     } else {
-        pTriangulo.textContent = "Não possivel fazer um triangulo com estes valores"
+        pTriangulo.textContent = "Não é possível fazer um triângulo com estes valores";
     }
 }
